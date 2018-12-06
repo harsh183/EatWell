8bf0ca8609ac3dd782181300ead59d31c29bb4ab
Colleen <ctm6@illinois.edu>
2018-12-05 22:57:38 -0600

    Working on writeToCalendar

--- app/src/main/java/com/example/harsh183/eatwell/MainActivity.java
+++ app/src/main/java/com/example/harsh183/eatwell/MainActivity.java
@@ -8,6 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 //import com.google.api.services.calendar.Calendar;
 
@@ -41,35 +47,35 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
-     *
+     * writes new event to the user's calendar using calculated times and dates
      * @param calendarId
      * @return should add event
      */
-     /*public String writeToCalendar(String calendarId) {
+     public String writeToCalendar(String calendarId) {
          Calendar service = new Calendar.Builder(httpTransport, jsonFactory, credentials)
                  .setApplicationName("applicationName").build();
 
-         Event meal = new Event;
+         Event meal = new Event();
          .setSummary("Eat well!"); //insert name of meal - will figure out how to get it later
          //import start time and date from calculator
          DateTime startDateTime = new DateTime("2015-05-28T09:00:00-07:00");
-         EventDateTime start = new EventDateTime()
+         EventDateTime startTime = new EventDateTime()
                  .setDateTime(startDateTime)
                  .setTimeZone("America/Chicago"); //add time zone support
-         meal.setStart(start);
+         meal.setStart(startTime);
          //import end time and date
          DateTime endDateTime = new DateTime("2015-05-28T17:00:00-07:00");
-         EventDateTime end = new EventDateTime()
+         EventDateTime endTime = new EventDateTime()
                  .setDateTime(endDateTime)
                  .setTimeZone("America/Chicago");
-         meal.setEnd(end);
+         meal.setEnd(endTime);
          //handle recurrence once we decide when we are eating
          String[] recurrence = new String[] {"RRULE:FREQ=NONE;COUNT=0"};
          meal.setRecurrence(Arrays.asList(recurrence));
          meal = service.events().insert(calendarId, meal).execute();
          System.out.print("Event created: %s\n", meal.getHtmlLink());
          POST http://www.google.com/calendar/feeds/jo@gmail.com/private/full
-     }*/
+     }
 
 
 
