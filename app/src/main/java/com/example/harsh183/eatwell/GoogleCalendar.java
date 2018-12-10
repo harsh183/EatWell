1a2a39f762c342717415cd598d7d0767937be419
Colleen <ctm6@illinois.edu>
2018-12-10 01:39:32 -0600

    Got getCalendar to theoretically work

--- app/src/main/java/com/example/harsh183/eatwell/GoogleCalendar.java
+++ app/src/main/java/com/example/harsh183/eatwell/GoogleCalendar.java
@@ -1,7 +1,9 @@
 package com.example.harsh183.eatwell;
 
 import android.util.Log;
-
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.model.CalendarList;
+import com.google.api.services.calendar.model.CalendarListEntry;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -18,20 +20,26 @@ public class GoogleCalendar {
     private JSONObject icsFile;
     /** Default logging tag for messages from the main activity. */
     private static final String TAG = "EatWell:GoogleCalendar";
-
     /** Request queue for our network requests. */
     private static RequestQueue requestQueue;
 
-    void callCalendarAPI() {
+
+    public void getCalendarId() {
         try {
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                     Request.Method.GET,
-                    "https://www.googleapis.com/calendar/" + calendarId + "/events",
+                    "https://www.googleapis.com/calendar/v3/users/me/calendarList",
                     null,
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(final JSONObject response) {
-                            icsFile = response; //get actual ICS file
+                            try {
+                                CalendarList listofCals = (CalendarList) response.get("items");
+                                calendarId = (String) listofCals.get("id");
+                            }
+                            catch (Exception e) {
+                                e.printStackTrace();
+                            }
                             Log.d(TAG, response.toString());
                         }
                     }, new Response.ErrorListener() {
@@ -46,10 +54,33 @@ public class GoogleCalendar {
         }
 
     }
-    public JSONObject getCalendar(String calendarIdInput) {
-        calendarId = calendarIdInput;
-        callCalendarAPI();
+    public void getCalendar(String calId) {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://www.googleapis.com/calendar/v3/calendars/" + calendarId + "/events",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public JSONObject returnEventList() {
+        getCalendarId();
+        getCalendar(calendarId);
         return icsFile;
     }
-    //TODO: public String outputWhenToEat()
+
 }
