69173a29bcc8660870913962f722eab857040ec9
Colleen <ctm6@illinois.edu>
2018-12-10 02:38:31 -0600

    Got getCalendar to theoretically work

--- app/src/main/java/com/example/harsh183/eatwell/GoogleCalendar.java
+++ app/src/main/java/com/example/harsh183/eatwell/GoogleCalendar.java
@@ -1,6 +1,9 @@
 package com.example.harsh183.eatwell;
 
 import android.util.Log;
+
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.model.CalendarList;
 import com.google.api.services.calendar.model.CalendarListEntry;
@@ -11,6 +14,26 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONObject;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * This class is where we have stored the methods that obtain the calendar through the Google calendar API.
@@ -22,6 +45,15 @@ public class GoogleCalendar {
     private static final String TAG = "EatWell:GoogleCalendar";
     /** Request queue for our network requests. */
     private static RequestQueue requestQueue;
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
+    private static final String CREDENTIALS_FILE_PATH = "/client_id.json";
 
 
     public void getCalendarId() {
@@ -54,7 +86,7 @@ public class GoogleCalendar {
         }
 
     }
-    public void getCalendar(String calId) {
+    public void getCalendarList(String calId) {
         try {
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                     Request.Method.GET,
@@ -77,10 +109,33 @@ public class GoogleCalendar {
         }
 
     }
-    public JSONObject returnEventList() {
+    public JSONObject getCalendar() {
+        getConsent();
         getCalendarId();
-        getCalendar(calendarId);
+        getCalendarList(calendarId);
         return icsFile;
     }
 
+    public void getConsent() {
+        try {
+            final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                    .setApplicationName("Eat Well")
+                    .build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        InputStream in = GoogleCalendar.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("online")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
 }
